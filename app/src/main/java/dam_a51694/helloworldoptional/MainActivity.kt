55c9472bc5e25513756bbab234d589addc236fdf
SofiaSofs <sofiadamotasalgado@gmail.com>
2026-03-03 18:52:31 +0000

    5.3 Building a System Info App
    showing information about the current build

--- app/src/main/java/dam_a51694/helloworldoptional/MainActivity.kt
+++ app/src/main/java/dam_a51694/helloworldoptional/MainActivity.kt
@@ -1,6 +1,8 @@
 package dam_a51694.helloworldoptional
 
+import android.os.Build
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,25 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val tvInfo = findViewById<TextView>(R.id.tvInfo) // encontra o TextView do layout pelo id
+        // criar um texto com várias linhas com a marca do dispositivo, modelo, marca comercial, tipo de build,
+        // utilizador que fez a build, versao base do android, numero interno da build, versao do SDK,
+        // versao android visivel e identificador da build
+        val info = """
+            Manufacturer: ${Build.MANUFACTURER} 
+            Model: ${Build.MODEL}
+            Brand: ${Build.BRAND}
+            Type: ${Build.TYPE}
+            User: ${Build.USER}
+            Base: ${Build.VERSION_CODES.BASE}
+            Incremental: ${Build.VERSION.INCREMENTAL}
+            SDK: ${Build.VERSION.SDK_INT}
+            Version Code: ${Build.VERSION.RELEASE}
+            Display: ${Build.DISPLAY}
+        """.trimIndent()
+        // mostrar o texto no TextView
+        tvInfo.text = info
+
     }
 }
